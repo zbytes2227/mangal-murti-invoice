0dbcf1b49704b25867c8193a0b6edae97c91a9a1
zbytes2227 <zbytes0@gmail.com>
2024-05-31 15:25:04 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Mangal Murti",
+  description: "Invoice Software",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar/>
+        <div className="md:pl-10 md:ml-36" style={{ marginLeft: '38vh'}}>
+        {children}
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js" defer  />
+      </body>
+    </html>
+  );
+}
